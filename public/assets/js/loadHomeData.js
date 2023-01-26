3ad25acb3c81fe45d2e3365f3c7ebdc9920e1f0e
Sylvio Tavora <sylvio@stavora.com.br>
2023-01-26 07:45:24 -0300

    Changes loadHomeData / Views

--- public/assets/js/loadHomeData.js
+++ public/assets/js/loadHomeData.js
@@ -4,37 +4,68 @@ import {swipe} from '/assets/js/swipeBanner.js';
     var bannerHome = document.querySelector('._bannerHome');    
     var recent = document.querySelector('._recent'); 
     var categoryCulture = document.querySelector('._category_culture'); 
+    var categoryBusiness = document.querySelector('._category_business'); 
+    var categoryLifeStyle = document.querySelector('._category_lifestyle'); 
     
-    const responses = await Promise.all([
-        await fetch('/banner/home',{method:'get'}),
-        await fetch('/trendings',{method:'get'}),
-        await fetch('/recent',{method:'get'}),
-        await fetch('/category/culture',{method:'get'}),
-    ]);
+    // const responses = await Promise.all([
+    //     await fetch('/banner/home',{method:'get'}),
+    //     await fetch('/trendings',{method:'get'}),
+    //     await fetch('/recent',{method:'get'}),
+    //     await fetch('/category/culture',{method:'get'}),
+    //     await fetch('/category/business',{method:'get'}),
+    //     await fetch('/category/lifestyle',{method:'get'}),
+    // ]);
 
-    const bannerHtml = await responses[0].text();
-    const trendingHtml = await responses[1].text();
-    const recentHtml = await responses[2].text();
-    const categoryCultureHtml = await responses[3].text();
+    // const bannerHtml = await responses[0].text();
+    // const trendingHtml = await responses[1].text();
+    // const recentHtml = await responses[2].text();
+    // const categoryCultureHtml = await responses[3].text();
+    // const categoryBusinessHtml = await responses[4].text();
+    // const categoryLifeStyleHtml = await responses[5].text();
 
-    bannerHome.innerHTML = bannerHtml;
-    recent.innerHTML = recentHtml;
+    // bannerHome.innerHTML = bannerHtml;
+    // recent.innerHTML = recentHtml;
 
-    var trending = document.querySelector('._trending'); 
-    trending.innerHTML = trendingHtml;
-    categoryCulture.innerHTML = categoryCultureHtml;
+    // var trending = document.querySelector('._trending'); 
+    // trending.innerHTML = trendingHtml;
 
-    // // !! Load Banner Home
-    // const dataBanner = await fetch('/banner/home',{method:'get'});
-    // const bannerHtml = await dataBanner.text();
-    // bannerHome.innerHTML = bannerHtml;    
+    // categoryCulture.innerHTML = categoryCultureHtml;
+    // categoryBusiness.innerHTML = categoryBusinessHtml;
+    // categoryLifeStyle.innerHTML = categoryLifeStyleHtml;
 
-    // // !! Load Trendings
-    // const dataTrending = await fetch('/trendings',{method:'get'});
-    // const trendingHtml = await dataTrending.text();
-    // trending.innerHTML = trendingHtml;     
+    // !! load banner home
+    const dataBanner = await fetch('/banner/home',{method:'get'});
+    const bannerHtml = await dataBanner.text();
+    bannerHome.innerHTML = bannerHtml;  
 
     swipe('.sliderFeaturedPosts');
+    
+    // !! Load recent    
+    const dataRecent = await fetch('/recent',{method:'get'});
+    const recentHtml = await dataRecent.text();
+    recent.innerHTML = recentHtml; 
+
+    // !! Load trendings
+    var trending = document.querySelector('._trending'); 
+    const dataTrending = await fetch('/trendings',{method:'get'});
+    const trendingHtml = await dataTrending.text();
+    trending.innerHTML = trendingHtml;  
+
+    // !! Catogory culture
+    const dataCategoryCulture = await fetch('/category/culture',{method:'get'});
+    const categoryCultureHtml = await dataCategoryCulture.text();
+    categoryCulture.innerHTML  = categoryCultureHtml;
+
+    // !! Catogory business
+    const dataCategoryBusiness = await fetch('/category/business',{method:'get'});
+    const categoryBusinessHtml = await dataCategoryBusiness.text();
+    categoryBusiness.innerHTML  = categoryBusinessHtml;
+
+    // !! Catogory lifestyle
+    const dataCategoryLifeStyle = await fetch('/category/lifestyle',{method:'get'});
+    const categoryLifeStyleHtml = await dataCategoryLifeStyle.text();
+    categoryCulture.innerHTML  = categoryLifeStyleHtml;
+
   }
 
   loadHomeData();
